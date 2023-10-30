a0c2e5619b43f6ef7e718d5b7221976b4a3bff8f
Christopher Robison <cdr@netoasis.net>
2023-10-30 12:14:22 -0700

    Initial checkin

---
+++ api.php
@@ -0,0 +1,62 @@
+<?php
+include('.env');
+
+$in = $_REQUEST;
+$out = array();
+$link = mysqli_connect($env->db->host, $env->db->user, $env->db->pass, "system");
+
+session_start();
+
+/* check connection */
+if (mysqli_connect_errno()) {
+     printf("Connect failed: %s\n", mysqli_connect_error());
+     exit();
+}
+
+$out = array();
+
+if ($in['x']) {
+    switch($in['x']) {
+        case "get_zones":
+            $out = getZones();
+            break;
+        case "get_record":
+        case "get_zone":
+            $out = getZone();
+            break;
+        case "get_all":
+            $out = getAllDNS();
+            break;
+        default:
+
+    }
+}
+
+if ($out) {
+    header("Content-Type: application/json");
+    print json_encode($out);
+}
+
+function getZones() {
+    global $link;
+    global $in;
+    $out = array();
+    $result = mysqli_query($link, "SELECT DISTINCT(zone) from dns");
+
+    while ($row = mysqli_fetch_object($result)) {
+        $out[] = $row->zone;
+    }
+    return $out;
+}
+
+function getZone() {
+    global $link;
+    global $in;
+    $out = array();
+    $result = mysqli_query($link, "SELECT * from dns where zone='{$in['zone']}'");
+
+    while ($row = mysqli_fetch_object($result)) {
+        $out[] = $row;
+    }
+    return $out;
+}
